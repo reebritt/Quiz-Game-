1e6f5780bef08eadad6a00c7fe9027496db2cc82
Ree Britt <reebritt@icloud.com>
2021-01-14 21:03:31 -0500

    setting up code

---
+++ Assets/javascript/questions.js
@@ -0,0 +1,53 @@
+
+
+
+var questionsArray = [
+    {
+      question: "The album songs in A Minor was recorded by what artist?",
+      choices: ["Beyonce", "Alicia Keys", "", ""],
+     // answer should have matching string from choices 
+     answer: "Alicia Keys"
+
+    },
+
+    {
+        question: "Which artist remade Roberta Flack's Killing Me Softly?",
+        choices: ["Jennifer Hudson", "Beyonce", "Lauren Hill", ""],
+       // answer should have matching string from choices 
+       answer: "Lauren Hill"
+  
+      },
+
+      {
+        question: "Which Michael Jackson Albumn was his best selling albumn?",
+        choices: ["Thriller", "", "", ""],
+       // answer should have matching string from choices 
+       answer: "Thriller"
+  
+      },
+
+      {
+        question: "Which groups hit song was "No Scrubs" ,
+        choices: ["Destiny's Child", "", "TLC", ""],
+       // answer should have matching string from choices 
+       answer: "TLC"
+  
+      },
+
+      {
+        question: "Which group was Jeffrey Osbourne the Lead singer of?" ,
+        choices: ["O'jays", "", "", "LTD"],
+       // answer should have matching string from choices 
+       answer: "LTD"
+  
+      },
+
+
+      {
+        question: "What state was mentioned in Glady's Knight's hit song - Midnight Train to Georgia?" ,
+        choices: ["Tennessee", "Georgia", "Mississippi", "New York"],
+       // answer should have matching string from choices 
+       answer: "Georgia"
+  
+      },
+]

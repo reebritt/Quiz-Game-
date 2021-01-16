3d05af206287e5b76a19791507392e2811d40025
Ree Britt <reebritt@icloud.com>
2021-01-16 12:22:34 -0500

    added scores highscores and updated html

--- Assets/javascript/questions.js
+++ Assets/javascript/questions.js
@@ -1,53 +1,52 @@
+var questions = [
+  {
+    question: "The album songs in A Minor was recorded by what artist?",
+    choices: ["Beyonce", "Alicia Keys", "", ""],
+   // answer should have matching string from choices 
+   answer: "Alicia Keys"
 
+  },
 
-
-var questionsArray = [
-    {
-      question: "The album songs in A Minor was recorded by what artist?",
-      choices: ["Beyonce", "Alicia Keys", "", ""],
+  {
+      question: "Which artist remade Roberta Flack's Killing Me Softly?",
+      choices: ["Jennifer Hudson", "Beyonce", "Lauren Hill", "Ashanti"],
      // answer should have matching string from choices 
-     answer: "Alicia Keys"
+     answer: "Lauren Hill"
 
     },
 
     {
-        question: "Which artist remade Roberta Flack's Killing Me Softly?",
-        choices: ["Jennifer Hudson", "Beyonce", "Lauren Hill", ""],
-       // answer should have matching string from choices 
-       answer: "Lauren Hill"
-  
-      },
+      question: "Which Michael Jackson Albumn was his best selling albumn?",
+      choices: ["Thriller", "Beat It", "", ""],
+     // answer should have matching string from choices 
+     answer: "Thriller"
 
-      {
-        question: "Which Michael Jackson Albumn was his best selling albumn?",
-        choices: ["Thriller", "", "", ""],
-       // answer should have matching string from choices 
-       answer: "Thriller"
-  
-      },
+    },
 
-      {
-        question: "Which groups hit song was "No Scrubs" ,
-        choices: ["Destiny's Child", "", "TLC", ""],
-       // answer should have matching string from choices 
-       answer: "TLC"
-  
-      },
+    {
+      question: "Which groups hit song was No Scrubs",
+      choices: ["Destiny's Child", "", "TLC", ""],
+     // answer should have matching string from choices 
+     answer: "TLC"
 
-      {
-        question: "Which group was Jeffrey Osbourne the Lead singer of?" ,
-        choices: ["O'jays", "", "", "LTD"],
-       // answer should have matching string from choices 
-       answer: "LTD"
-  
-      },
+    },
+
+    {
+      question: "Which group was Jeffrey Osbourne the Lead singer of?" ,
+      choices: ["O'jays", "", "", "LTD"],
+     // answer should have matching string from choices 
+     answer: "LTD"
+
+    },
 
 
-      {
-        question: "What state was mentioned in Glady's Knight's hit song - Midnight Train to Georgia?" ,
-        choices: ["Tennessee", "Georgia", "Mississippi", "New York"],
-       // answer should have matching string from choices 
-       answer: "Georgia"
-  
-      },
-]
+    {
+      question: "What state was mentioned in Glady's Knight's hit song - Midnight Train to Georgia?" ,
+      choices: ["Tennessee", "Georgia", "Mississippi", "New York"],
+     // answer should have matching string from choices 
+     answer: "Georgia"
+
+    },
+];
+
+
